1377e56baedc16d3f7efcb89df539cec1c337bcc
AndoniLatorreGalarraga <andonilatorregalarraga@gmail.com>
2021-12-01 21:45:36 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,24 @@
+# part 1
 with open('1.txt', 'r', encoding='utf-8') as fp:
-    str = fp.readline()
-    o = int(str)
+    s = fp.readline()
+    o = int(s)
     ans = 0
-    for str in fp.readlines():
-        n = int(str)
+    for s in fp.readlines():
+        n = int(s)
         if n > o:
             ans += 1
         o = n
-print(ans)
+print(ans) #1387
+# part 2
+with open('1.txt', 'r', encoding='utf-8') as fp:
+    l = []
+    for _ in range(3):
+        l.append(int(fp.readline()))
+    ans = 0
+    for s in fp.readlines():
+        n = int(s)
+        if l[0] < n: #l[0]+l[1]+l[2] < l[1]+l[2]+n
+            ans += 1
+        l.pop(0)
+        l.append(n)
+print(ans) #1362
